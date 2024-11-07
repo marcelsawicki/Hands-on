d4e95bb27944b4b0968bfbaed5a69ec5ca023bfb
Marcel Sawicki <marcel.sawicki@insert.com.pl>
2024-11-07 17:35:33 +0100

    Snacks

---
+++ Angular/two-way-data-communication/snacks/src/app/snack.ts
@@ -0,0 +1,15 @@
+export interface Snack {
+    name: string;
+    price: number;
+  }
+  
+  // Hard-coded data
+  export const SNACKS = [
+    { name: 'popcorn', price: 2.0 },
+    { name: 'peanuts', price: 3.0 },
+    { name: 'mixed nuts', price: 5.0 },
+    { name: 'crackers', price: 2.5 },
+    { name: 'ice cream', price: 3.95 },
+    { name: 'corn nuts', price: 2.29 },
+  ];
+  
