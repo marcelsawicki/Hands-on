255c2481c24f1310883364ce5fa2074f876d3897
Marcel Sawicki <marcel.sawicki@insert.com.pl>
2024-11-29 14:56:43 +0100

    FilterComponent

--- Angular/two-way-data-communication/snacks2/src/app/snack/snack.component.ts
+++ Angular/two-way-data-communication/snacks2/src/app/snack/snack.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { SnackListComponent } from '../snack-list/snack-list.component';
 import { FormsModule } from '@angular/forms';
+import { FilterComponent } from "../filter/filter.component";
 
 @Component({
   selector: 'app-snack',
   templateUrl: './snack.component.html',
-  styleUrl: './snack.component.css'
+  styleUrl: './snack.component.css',
+  imports: [FilterComponent]
 })
 export class SnackComponent {
   pageTitle = 'Menu Options';
